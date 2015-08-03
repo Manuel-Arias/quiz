9c03592a783d24c6c4e13c336e31417d2975eb75
Manuel Arias <manuelfc.arias@gmail.com>
2015-08-03 21:20:15 +0200

    Ejercicio p2p 8

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -57,7 +57,7 @@ exports.create = function(req, res){
 				} else{
 					// guarda en DB los campos pregunta y respuesta en quiz
 					quiz
-					.save({fields: ["pregunta", "respuesta"]})
+					.save({fields: ["pregunta", "respuesta", "tema"]})
 					.then(function(){
 						res.redirect('/quizes')}) // redireccion HTTP (URL relativo) lista de preguntas	
 				}
@@ -75,7 +75,8 @@ exports.edit = function(req, res){
 exports.update = function(req, res){
 	req.quiz.pregunta = req.body.quiz.pregunta;
 	req.quiz.respuesta = req.body.quiz.respuesta;
-
+	req.quiz.tema = req.body.quiz.tema;
+	
 	req.quiz
 	.validate()
 	.then(
@@ -84,7 +85,7 @@ exports.update = function(req, res){
 				res.render('quizes/edit', {quiz: req.quiz, errors: err.errors});
 			} else{
 				req.quiz //save: guarda campos pregunta y respuesta en DB
-				.save( {fields: ["pregunta", "respuesta"]})
+				.save( {fields: ["pregunta", "respuesta", "tema"]})
 				.then( function(){ res.redirect('/quizes');});
 			}	// Redireccion HTTP a lista de preguntas (URL relativo)
 		}
